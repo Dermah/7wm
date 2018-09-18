7a7c4eb570c21d9671bb2e626de4ee428d7663d0
Sputnik <inquiry@dermah.com>
2018-09-18 23:32:38 +1000

    Test that the logo component can be styled

--- src/components/Channel7Logo/test.jsx
+++ src/components/Channel7Logo/test.jsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
+import styled from "styled-components";
 
 import Channel7Logo from "./";
 
@@ -7,5 +9,14 @@ describe("Channel7Logo", () => {
   test("snapshot test", () => {
     const tree = renderer.create(<Channel7Logo />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("can be styled", () => {
+    const WrappedLogo = styled(Channel7Logo)`
+      margin-left: 1em;
+    `;
+
+    const renderedLogo = shallow(<WrappedLogo />);
+    expect(renderedLogo.hasClass(WrappedLogo.styledComponentId)).toBe(true);
   });
 });
