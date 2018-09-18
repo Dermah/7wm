5278f251b77063acf10834d8f3fdb07322011e96
Sputnik <inquiry@dermah.com>
2018-09-18 22:47:38 +1000

    Style the tile's surroundings to match composite

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MediaTile from './MediaTile';
+import React, { Component } from "react";
+import "./App.css";
+import MediaTile from "./MediaTile";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <MediaTile title="Home And Away"/>
+        <MediaTile title="Home And Away" />
       </div>
     );
   }
