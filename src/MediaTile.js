daaf7fa4e184d0075c86d975fff8fd9ddb0e8de0
Sputnik <inquiry@dermah.com>
2018-09-18 22:42:40 +1000

    Add a positioned title bar to the media tile

--- src/MediaTile.js
+++ src/MediaTile.js
@@ -4,7 +4,26 @@ import styled from "styled-components";
 const TileBase = styled.div`
   width: 630px;
   height: 354px;
+  position: relative;
   background-image: url("./tile.jpg");
 `;
 
-export default ({ title }) => <TileBase>{title}</TileBase>;
+const TileTitle = styled.h3`
+  font-weight: normal;
+  font-size: 1.5em;
+  color: white;
+  text-align: left;
+  background: rgba(0, 0, 0, 0.25);
+
+  width: 100%;
+  margin: 0;
+  padding: 1em;
+  position: absolute;
+  bottom: 0;
+`;
+
+export default ({ title }) => (
+  <TileBase>
+    <TileTitle>{title}</TileTitle>
+  </TileBase>
+);
